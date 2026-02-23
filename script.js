240885e97fdb934c6dfd1ee9138936d9f2322b72
wigor <wpgameprey@gmail.com>
2026-02-23 15:41:35 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const nav = document.querySelector(".nav");
+
+mobileMenu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
